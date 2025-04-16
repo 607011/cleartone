3c9d30b871d8452de6bed5c16fff758b87625406
Oliver Lau <oliver@ersatzworld.net>
2025-04-16 14:47:03 +0200

    Images size optimized before upload.

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -3,13 +3,16 @@
 source .env
 
 DIST=dist
-mkdir -p ${DIST}
+mkdir -p ${DIST}/static/images
 echo Copying files ...
 cp -r index.html app.js noise.js ${DIST}
+cp -r static/images ${DIST}/static/
 echo Minifying ...
 minify ${DIST}/index.html -o ${DIST}/index.html
 minify ${DIST}/app.js -o ${DIST}/app.js
 minify ${DIST}/noise.js -o ${DIST}/noise.js
+echo Optimizing images ...
+optipng -quiet -o7 ${DIST}/images/*.png
 echo Syncing to ${DST} ...
 cd ${DIST}
 rsync --exclude '.DS_Store' -Rrav . ${DST}

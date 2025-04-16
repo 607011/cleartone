8bc93814b889369a0e9092e9abb58396fc2b341a
Oliver Lau <oliver@ersatzworld.net>
2025-04-16 07:46:04 +0200

    Updated deployment script to reflect changes in 944b2da1a97ee3fc7eecc25baa0a2030364f226a.

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -5,10 +5,11 @@ source .env
 DIST=dist
 mkdir -p ${DIST}
 echo Copying files ...
-cp -r index.html app.js ${DIST}
+cp -r index.html app.js noise.js ${DIST}
 echo Minifying ...
 minify ${DIST}/index.html -o ${DIST}/index.html
 minify ${DIST}/app.js -o ${DIST}/app.js
+minify ${DIST}/noise.js -o ${DIST}/noise.js
 echo Syncing to ${DST} ...
 cd ${DIST}
 rsync --exclude '.DS_Store' -Rrav . ${DST}
